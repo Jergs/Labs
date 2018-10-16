6289ddeedfeaf8dcfa4a0c2d31b7eb3a87e1304a
Jergs <36529973+Jergs@users.noreply.github.com>
2018-10-16 09:45:36 +0300

    firstlab

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form class="" action="process.php" method="POST">
+		<div class="container">
+			<label for=""> Sign up</label>
+			<input type="text" name="login" placeholder="login" value="">
+			<input type="password" name="password" placeholder="password" value="">
+			<a href="GuestPage.php"> Login as a guest</a>
+			<button type="submit" class="btn" name="log">Login</button>
+		</div>	
+	</form>
+</body>
+</html>
+			
